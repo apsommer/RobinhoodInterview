a1a0afd1cc6f4fdd1197e849398f1186d7050be5
Drew Sommer <drew@sommerengineering.com>
2025-03-12 12:24:08 +0100

    fix: Build config conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
-    // id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -70,9 +69,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    // implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.kotlinx.serialization.json)
 }
